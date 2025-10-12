dca987cd60d6eda8ebd96e9f0519748576249f6e
Yousaf Wazir <umbrellaman612@gmail.com>
2025-10-12 15:10:54 +0100

    fixed flex scroll problem
    - now the file explorer scolls in itself
    - all flex 1 parents need min height to fix

--- ui/src/features/file-explorer/file-explorer.component.ts
+++ ui/src/features/file-explorer/file-explorer.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+} from '@angular/core';
 
 @Component({
   selector: 'app-file-explorer',
   imports: [],
   templateUrl: './file-explorer.component.html',
-  styleUrl: './file-explorer.component.css'
+  styleUrl: './file-explorer.component.css',
 })
 export class FileExplorerComponent {
+  /**
+   * The directory to show in file explorer
+   */
+  selectedDirectory: string | null = null;
 
 }
