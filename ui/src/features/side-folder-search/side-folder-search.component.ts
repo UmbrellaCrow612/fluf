85c61adb0d169a7ea2f0ebaa71e4aef155902622
Yousaf Wazir <umbrellaman612@gmail.com>
2025-11-05 12:05:36 +0000

    search works
    - todo just add html inputs template for other options
    - todo add a list item compo render for each item

--- ui/src/features/side-folder-search/side-folder-search.component.ts
+++ ui/src/features/side-folder-search/side-folder-search.component.ts
@@ -1,9 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { getElectronApi } from '../../utils';
+import { ContextService } from '../app-context/app-context.service';
 
 @Component({
   selector: 'app-side-folder-search',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './side-folder-search.component.html',
   styleUrl: './side-folder-search.component.css',
 })
-export class SideFolderSearchComponent {}
+export class SideFolderSearchComponent {
+  private readonly api = getElectronApi();
+  private readonly contextService = inject(ContextService);
+  private readonly selectedDir =
+    this.contextService.getSnapshot().selectedDirectoryPath;
+
+  searchInputControl = new FormControl('', {
+    validators: [Validators.required],
+  });
+
+  expandOptions = true;
+
+  /**
+   * Object used as two way binding
+   */
+  fosOptions: fosOptions = {
+    caseInsensitive: true,
+    countOnly: false,
+    debug: false,
+    depth: 0,
+    exclude: [],
+    includeHidden: false,
+    limit: 0,
+    open: false,
+    partial: true,
+    preview: false,
+    sort: 'name',
+  };
+
+  async submit(event: Event) {
+    event.preventDefault();
+
+    if (!this.searchInputControl.valid) {
+      return;
+    }
+
+    let results = await this.api.fos(
+      undefined,
+      this.searchInputControl.value!,
+      this.selectedDir!,
+      {
+        caseInsensitive: true,
+        partial: true,
+      }
+    );
+
+    console.log(results);
+  }
+}
