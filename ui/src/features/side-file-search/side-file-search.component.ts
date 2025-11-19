fab01da6c7471d9b9094d2c345f756017d00c622
Yousaf Wazir <umbrellaman612@gmail.com>
2025-11-19 09:56:48 +0000

    some styling

--- ui/src/features/side-file-search/side-file-search.component.ts
+++ ui/src/features/side-file-search/side-file-search.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ContextService } from '../app-context/app-context.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-side-file-search',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './side-file-search.component.html',
   styleUrl: './side-file-search.component.css',
 })
 export class SideFileSearchComponent {
+  private readonly appContext = inject(ContextService);
+  private readonly selectedDir =
+    this.appContext.getSnapshot().selectedDirectoryPath;
 
+  showOptions = false;
+
+  searchFormOptions = new FormGroup({
+    partial: new FormControl(false),
+    ignoreCase: new FormControl(false),
+    open: new FormControl(false),
+    limit: new FormControl(0),
+    depth: new FormControl(0),
+
+    ext: new FormControl<string[] | null>(null),
+    excludeExt: new FormControl<string[] | null>(null),
+    excludeDir: new FormControl<string[] | null>(null),
+
+    minSize: new FormControl(0),
+    maxSize: new FormControl(0),
+    sizeType: new FormControl<'B' | 'KB'>('B'),
+
+    modifiedBefore: new FormControl(''),
+    modifiedAfter: new FormControl(''),
+
+    hidden: new FormControl(false),
+    count: new FormControl(false),
+    regex: new FormControl(false),
+  });
 }
