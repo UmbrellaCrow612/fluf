cc801c44a37b65f91e9bc48515b2cd7a75ce5b64
Yousaf Wazir <umbrellaman612@gmail.com>
2025-11-20 09:24:11 +0000

    more

--- ui/src/features/app-context/type.ts
+++ ui/src/features/app-context/type.ts
@@ -26,3 +26,94 @@ export type fileEditorBottomActiveElement =
  */
 export type contextMenuActiveElement =
   /** Displays when a right click is done in file explorer and on a specific file node*/ 'file-explorer-file-node-context-menu';
+
+/**
+ * Represents application wide context that persists between sessions
+ */
+export type AppContext = {
+  /**
+   * Current active sidebar element
+   */
+  sideBarActiveElement: sideBarActiveElement;
+
+  /**
+   * List of nodes read from the selected directory
+   */
+  directoryFileNodes: Array<fileNode> | null;
+
+  /**
+   * Folder path selected in the editor
+   */
+  selectedDirectoryPath: string | null;
+
+  /**
+   * The current focused / last clicked or last edited file (from editor) in the file explorer tree
+   */
+  fileExplorerActiveFileOrFolder: fileNode | null;
+
+  /**
+   * Represents whether a file or folder creator is active
+   */
+  isCreateFileOrFolderActive: boolean | null;
+
+  /**
+   * Indicates if it should refresh / reread nodes and update the current nodes with updated folder nodes -
+   * keeps expanded state and adds / removes children based on new state
+   */
+  refreshDirectory: boolean | null;
+
+  /**
+   * List of open files in the editor to show in the open file tab bar
+   */
+  openFiles: fileNode[] | null;
+
+  /**
+   * The current file being displayed in the text file editor
+   */
+  currentOpenFileInEditor: fileNode | null;
+
+  /**
+   * Indicates if it should show the file editor bottom section, which contains the terminal, problems, etc.
+   */
+  displayFileEditorBottom: boolean | null;
+
+  /**
+   * The current active element in the file editor bottom container
+   */
+  fileEditorBottomActiveElement: fileEditorBottomActiveElement | null;
+
+  /**
+   * List of active shells
+   */
+  shells: shellInformation[] | null;
+
+  /**
+   * The current active shell to see output and input cmds in
+   */
+  currentActiveShellId: string | null;
+
+  /**
+   * Use a sub to listen when this is fired off - used when file explorer is resized or open file bottom is resized
+   */
+  isEditorResize: boolean | null;
+
+  /**
+   * The current context menu that is showing, if there is a value then said context menu will apppear else the context menu will be closed
+   */
+  currentActiveContextMenu: contextMenuActiveElement | null;
+
+  /**
+   * The x and y position pprovided to where to render the context menu
+   */
+  currentActiveContextMenuPosition: { x: number; y: number } | null;
+
+  /**
+   * Any data you want to pass to the context menu note validation must be done in the context menu itself to make sure the correct data was passed
+   */
+  currentActiveContextMenuData: Object | null;
+};
+
+/**
+ * The callback that runs when a field changes that you sub to
+ */
+export type AppContextCallback = (ctx: AppContext) => void | Promise<void>;
--- ui/src/features/context-menu/context-menu.component.ts
+++ ui/src/features/context-menu/context-menu.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { ContextService } from '../app-context/app-context.service';
 
 @Component({
   selector: 'app-context-menu',
@@ -6,6 +7,11 @@ import { Component } from '@angular/core';
   templateUrl: './context-menu.component.html',
   styleUrl: './context-menu.component.css',
 })
-export class ContextMenuComponent {
+export class ContextMenuComponent implements OnInit, OnDestroy {
+  private readonly appContext = inject(ContextService)
+  ngOnInit(): void {}
 
+  ngOnDestroy(): void {
+    // Clean up current active to be null
+  }
 }
