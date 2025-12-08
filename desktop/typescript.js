014b5ad5ae6bb1e8c3cfc030a600d9b8eab73e36
Yousaf Wazir <umbrellaman612@gmail.com>
2025-12-08 13:13:00 +0000

    Update typescript.js

--- desktop/typescript.js
+++ desktop/typescript.js
@@ -2,7 +2,6 @@
     Contains all the code to allow ts typescript language support for the UI editor
 */
 
-const path = require("path");
 const { getTsServerPath } = require("./packing");
 const fs = require("fs");
 const { spawn } = require("child_process");
@@ -12,19 +11,28 @@ const { spawn } = require("child_process");
  */
 let childSpawnRef = null;
 
+let stdoutBuffer = "";
+
+/**
+ * Parse the stdout from ts server
+ * @param {string} buffer
+ * @returns {Object}
+ */
+const parseStdout = (buffer) => {
+  return {};
+};
+
 const startTsServer = () => {
-  const p = getTsServerPath();
-  if (!fs.existsSync(p)) {
-    console.error("TS server not found at:", p);
+  const path = getTsServerPath();
+  if (!fs.existsSync(path)) {
+    console.error("TS server not found at:", path);
     return;
   }
 
-  childSpawnRef = spawn("node", [p], {
-    stdio: ["pipe", "pipe", "pipe"],
-  });
+  childSpawnRef = spawn("node", [path]);
 
   childSpawnRef.stdout.on("data", (data) => {
-    console.log("TS Server stdout:", data.toString());
+    stdoutBuffer += data.toString();
   });
 
   childSpawnRef.stderr.on("data", (data) => {
@@ -35,7 +43,7 @@ const startTsServer = () => {
     console.log("TS Server exited with code", code);
   });
 
-  console.log("TS Server started at", p);
+  console.log("TS Server started at", path);
 };
 
 /**
@@ -51,8 +59,6 @@ const stopTsServer = () => {
   }
 };
 
-
-
 /**
  * Register all TS main events listeners
  * @param {import("electron").IpcMain} ipcMain
@@ -61,18 +67,14 @@ const registerTsListeners = (ipcMain) => {
   ipcMain.on("tsserver-send", (event, message) => {
     if (childSpawnRef) {
       childSpawnRef.stdin.write(JSON.stringify(message) + "\n");
-
-      childSpawnRef.stdout.once("data", (data) => {
-        let parsed;
-        try {
-          parsed = JSON.parse(data.toString());
-        } catch {
-          parsed = data.toString();
-        }
-        event.sender.send("tsserver-response", parsed);
-      });
+      let r = parseStdout(stdoutBuffer);
+      if (r) {
+        event.sender.send("tsserver-respponse", r);
+      }
     }
   });
+
+  // on data out pparse stdout then send event to tsserver-response with parsed stdout of TS server
 };
 
 module.exports = { startTsServer, stopTsServer, registerTsListeners };
