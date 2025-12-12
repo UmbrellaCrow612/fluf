4ddd2e6ef4a98ef5d27440c024987fad4a052485
Yousaf Wazir <umbrellaman612@gmail.com>
2025-12-12 09:03:14 +0000

    made lsp client

--- ui/src/features/language/language.service.ts
+++ ui/src/features/language/language.service.ts
@@ -1,6 +1,12 @@
+import { EditorState } from '@codemirror/state';
 import { Injectable } from '@angular/core';
-import { LanguageServer } from './type';
+import {
+  LanguageServer,
+  ILanguageService,
+  LanguageServiceCallback,
+} from './type';
 import { getElectronApi } from '../../utils';
+import { mapTypescriptDiagnosticToCodeMirrorDiagnostic } from '../text-file-editor/typescript';
 
 /**
  * Service used to send and respond to backend language service for specific language servers such as TS server ot HTML etc in a central way
@@ -8,9 +14,77 @@ import { getElectronApi } from '../../utils';
 @Injectable({
   providedIn: 'root',
 })
-export class LanguageService {
+export class LanguageService implements ILanguageService {
   private readonly api = getElectronApi();
 
+  Open = (
+    filePath: string,
+    fileContent: string,
+    langServer: LanguageServer
+  ) => {
+    switch (langServer) {
+      case 'js/ts':
+        this.api.tsServer.openFile(filePath, fileContent);
+        break;
 
-  
+      default:
+        break;
+    }
+  };
+
+  Completion = (
+    filePath: string,
+    lineNumber: number,
+    lineOffest: number,
+    langServer: LanguageServer
+  ) => {
+    switch (langServer) {
+      case 'js/ts':
+        this.api.tsServer.completion(filePath, lineNumber, lineOffest);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  Edit = (
+    filePath: string,
+    fileContent: string,
+    langServer: LanguageServer
+  ) => {
+    switch (langServer) {
+      case 'js/ts':
+        this.api.tsServer.editFile(filePath, fileContent);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  OnResponse = (
+    langServer: LanguageServer,
+    editorState: EditorState,
+    callback: LanguageServiceCallback
+  ) => {
+    switch (langServer) {
+      case 'js/ts':
+        let unsub = this.api.tsServer.onResponse((data) => {
+          let d = mapTypescriptDiagnosticToCodeMirrorDiagnostic(
+            [data],
+            editorState
+          );
+
+          callback(d);
+        });
+
+        return () => {
+          unsub();
+        };
+
+      default:
+        return () => {};
+    }
+  };
 }
