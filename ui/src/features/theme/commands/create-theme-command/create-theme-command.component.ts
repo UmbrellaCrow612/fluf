1b57f7728337a2e88ac2cf24b1ad18b87236d4f4
Yousaf Wazir <umbrellaman612@gmail.com>
2026-01-01 13:27:12 +0000

    basics done

--- ui/src/features/theme/commands/create-theme-command/create-theme-command.component.ts
+++ ui/src/features/theme/commands/create-theme-command/create-theme-command.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { cssVariables } from '../../../../gen/cssVars';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-theme-command',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create-theme-command.component.html',
   styleUrl: './create-theme-command.component.css',
 })
 export class CreateThemeCommandComponent {
+  /**
+   * Represents a given css variable property and it's value mapped from cssVariables
+   */
+  cssVars: { property: string; value: string }[] = [];
 
+  constructor() {
+    this.cssVars = Object.entries(cssVariables).map(([property, value]) => ({
+      property,
+      value,
+    }));
+  }
+
+  saveTheme() {
+    // call save to explorer api to promt user to name it and save it somehwre then we can load it later
+  }
 }
