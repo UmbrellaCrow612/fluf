77faabea4ea3034102297eb005b902e63f4bf518
Yousaf Wazir <umbrellaman612@gmail.com>
2026-01-26 14:08:35 +0000

    top bar

---
+++ ui/src/FileX/file-x-top-bar/file-x-top-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-x-top-bar',
+  imports: [],
+  templateUrl: './file-x-top-bar.component.html',
+  styleUrl: './file-x-top-bar.component.css',
+})
+export class FileXTopBarComponent {
+
+}
--- ui/src/FileX/file-x.component.ts
+++ ui/src/FileX/file-x.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FileXTopBarComponent } from "./file-x-top-bar/file-x-top-bar.component";
 
 /**
  * Our custom built file explorer application similar to windows file explorer built inside the IDE to offer fast file exploration.
  */
 @Component({
   selector: 'app-file-x',
-  imports: [],
+  imports: [FileXTopBarComponent],
   templateUrl: './file-x.component.html',
   styleUrl: './file-x.component.css',
 })
