e2f4d60c9dee8e238bc553fcbdc22edb08ed918d
Yousaf Wazir <umbrellaman612@gmail.com>
2026-01-26 19:06:29 +0000

    work

--- ui/src/FileX/file-x.component.ts
+++ ui/src/FileX/file-x.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FileXTopBarComponent } from "./file-x-top-bar/file-x-top-bar.component";
+import { FileXTabsComponent } from "./file-x-tabs/file-x-tabs.component";
 
 /**
  * Our custom built file explorer application similar to windows file explorer built inside the IDE to offer fast file exploration.
  */
 @Component({
   selector: 'app-file-x',
-  imports: [FileXTopBarComponent],
+  imports: [FileXTopBarComponent, FileXTabsComponent],
   templateUrl: './file-x.component.html',
   styleUrl: './file-x.component.css',
 })
--- ui/src/editor/file-explorer/file-explorer-file-node-context-menu/file-explorer-file-node-context-menu.component.ts
+++ ui/src/editor/file-explorer/file-explorer-file-node-context-menu/file-explorer-file-node-context-menu.component.ts
@@ -2,6 +2,7 @@ import { Component, computed, inject, OnInit } from '@angular/core';
 import { EditorInMemoryContextService } from '../../app-context/editor-in-memory-context.service';
 import { fileNode } from '../../../gen/type';
 import { getElectronApi } from '../../../utils';
+import { FileXContextService } from '../../../FileX/file-x-context/file-x-context.service';
 
 @Component({
   selector: 'app-file-explorer-file-node-context-menu',
@@ -10,11 +11,14 @@ import { getElectronApi } from '../../../utils';
   styleUrl: './file-explorer-file-node-context-menu.component.css',
 })
 export class FileExplorerFileNodeContextMenuComponent implements OnInit {
-  private readonly inMemoryContextService = inject(EditorInMemoryContextService);
+  private readonly inMemoryContextService = inject(
+    EditorInMemoryContextService,
+  );
   private readonly contextMenuFileNode = computed(
     () => this.inMemoryContextService.currentActiveContextMenu()?.data,
   );
   private readonly api = getElectronApi();
+  private readonly fileXCtx = inject(FileXContextService);
 
   error: string | null = null;
 
@@ -38,6 +42,21 @@ export class FileExplorerFileNodeContextMenuComponent implements OnInit {
   }
 
   async openFileX() {
+    let node = this.contextMenuFileNode() as fileNode;
+    if (!node || !node?.path) {
+      this.error = 'Invaliud data passed';
+      return;
+    }
+
+    let tabs = this.fileXCtx.tabs();
+    if (tabs.length == 0) {
+      tabs.push({
+        baseDirectoryPath: node.path,
+        items: [],
+        name: node.name,
+      });
+      this.fileXCtx.tabs.set(structuredClone(tabs));
+    }
     await this.api.fileXApi.open();
     this.inMemoryContextService.currentActiveContextMenu.set(null);
   }
