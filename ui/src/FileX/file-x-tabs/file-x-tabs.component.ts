da5a034659a7c6df6492ac229871d7ce3d66289f
Yousaf Wazir <umbrellaman612@gmail.com>
2026-01-27 14:20:35 +0000

    more

--- ui/src/FileX/file-x-tabs/file-x-tabs.component.ts
+++ ui/src/FileX/file-x-tabs/file-x-tabs.component.ts
@@ -1,5 +1,7 @@
 import { Component, computed, inject } from '@angular/core';
 import { FileXContextService } from '../file-x-context/file-x-context.service';
+import { FileXTab } from '../file-x-context/type';
+import { getElectronApi } from '../../utils';
 
 @Component({
   selector: 'app-file-x-tabs',
@@ -9,7 +11,42 @@ import { FileXContextService } from '../file-x-context/file-x-context.service';
 })
 export class FileXTabsComponent {
   private readonly ctx = inject(FileXContextService);
+  private readonly api = getElectronApi();
 
   tabs = computed(() => this.ctx.tabs());
   activeDir = computed(() => this.ctx.currentActiveDirectoryTab());
+
+  /**
+   * Change the active directory when a tab item is clicked
+   * @param item The item clicked
+   */
+  changeActiveDirectory(item: FileXTab) {
+    this.ctx.currentActiveDirectoryTab.set(item.baseDirectoryPath);
+  }
+
+  /**
+   * Removes the given tab item from the list of active tabs and moves it to the next one
+   * @param item The item to remove
+   * @param event Angular event
+   */
+  removeDirectoryTabItem(event: Event, item: FileXTab) {
+    event.stopPropagation();
+
+    let tabs = this.ctx.tabs();
+    let filetredTabs = tabs.filter(
+      (x) => x.baseDirectoryPath !== item.baseDirectoryPath,
+    );
+
+    if (filetredTabs.length > 0) {
+      this.ctx.tabs.set(structuredClone(filetredTabs));
+
+      let next = filetredTabs[0];
+      this.ctx.currentActiveDirectoryTab.set(next.baseDirectoryPath);
+    } else {
+      this.ctx.tabs.set([]);
+      this.ctx.currentActiveDirectoryTab.set(null);
+
+      this.api.chromeWindowApi.close();
+    }
+  }
 }
