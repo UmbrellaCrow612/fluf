791dcb6ab660cca027997c3d496ed3415ebe6458
Yousaf Wazir <umbrellaman612@gmail.com>
2026-01-30 10:31:06 +0000

    tool bar items

--- ui/src/FileX/file-x-tool-bar/file-x-tool-bar.component.ts
+++ ui/src/FileX/file-x-tool-bar/file-x-tool-bar.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { FileXToolBarDirectoryPathViewerComponent } from "../file-x-tool-bar-directory-path-viewer/file-x-tool-bar-directory-path-viewer.component";
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-file-x-tool-bar',
-  imports: [FileXToolBarDirectoryPathViewerComponent],
+  imports: [FileXToolBarDirectoryPathViewerComponent, MatIconModule, MatButtonModule, MatTooltipModule],
   templateUrl: './file-x-tool-bar.component.html',
   styleUrl: './file-x-tool-bar.component.css',
 })
