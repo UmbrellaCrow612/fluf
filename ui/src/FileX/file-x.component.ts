df126eda4a32a9eee23cc3af949ba0e609a61c78
Yousaf Wazir <umbrellaman612@gmail.com>
2026-01-30 16:58:36 +0000

    tab items

--- ui/src/FileX/file-x.component.ts
+++ ui/src/FileX/file-x.component.ts
@@ -3,13 +3,14 @@ import { FileXTopBarComponent } from "./file-x-top-bar/file-x-top-bar.component"
 import { FileXToolBarComponent } from "./file-x-tool-bar/file-x-tool-bar.component";
 import { FileXQuickAccessFoldersComponent } from "./file-x-quick-access-folders/file-x-quick-access-folders.component";
 import { FileXDirectoryContentComponent } from "./file-x-directory-content/file-x-directory-content.component";
+import { FileXTabsComponent } from "./file-x-tabs/file-x-tabs.component";
 
 /**
  * Our custom built file explorer application similar to windows file explorer built inside the IDE to offer fast file exploration.
  */
 @Component({
   selector: 'app-file-x',
-  imports: [FileXTopBarComponent, FileXToolBarComponent, FileXQuickAccessFoldersComponent, FileXDirectoryContentComponent],
+  imports: [FileXTopBarComponent, FileXToolBarComponent, FileXQuickAccessFoldersComponent, FileXDirectoryContentComponent, FileXTabsComponent],
   templateUrl: './file-x.component.html',
   styleUrl: './file-x.component.css',
 })
