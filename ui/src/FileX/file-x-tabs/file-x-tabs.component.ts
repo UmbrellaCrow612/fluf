281c4bf475f6f30fbf7a64d84cfc59c13e516950
Yousaf Wazir <umbrellaman612@gmail.com>
2026-01-31 11:35:13 +0000

    more

--- ui/src/FileX/file-x-tabs/file-x-tabs.component.ts
+++ ui/src/FileX/file-x-tabs/file-x-tabs.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { MatIcon } from "@angular/material/icon";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from "@angular/material/icon";
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-file-x-tabs',
-  imports: [MatIcon],
+  imports: [MatIconModule, MatButtonModule, MatTooltipModule],
   templateUrl: './file-x-tabs.component.html',
   styleUrl: './file-x-tabs.component.css',
 })
