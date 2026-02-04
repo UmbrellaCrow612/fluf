9beb5a561d234a221b9b11f6e27eada4e8106330
Yousaf Wazir <umbrellaman612@gmail.com>
2026-02-04 09:39:42 +0000

    double click into item

--- ui/src/windows/FileX/file-x-directory-content/file-x-directory-content-item/file-x-directory-content-item.component.ts
+++ ui/src/windows/FileX/file-x-directory-content/file-x-directory-content-item/file-x-directory-content-item.component.ts
@@ -1,6 +1,8 @@
-import { Component, input } from '@angular/core';
+import { FileXContextService } from './../../file-x-context/file-x-context.service';
+import { Component, inject, input } from '@angular/core';
 import { fileNode } from '../../../../gen/type';
-import { MatIcon } from "@angular/material/icon";
+import { MatIcon } from '@angular/material/icon';
+import { ChangeActiveDirectory } from '../../utils';
 
 @Component({
   selector: 'app-file-x-directory-content-item',
@@ -9,8 +11,26 @@ import { MatIcon } from "@angular/material/icon";
   styleUrl: './file-x-directory-content-item.component.css',
 })
 export class FileXDirectoryContentItemComponent {
+  private readonly fileXContextService = inject(FileXContextService);
+
   /**
-   * The given node to render as a itemF
+   * The given node to render as a item
    */
   fileNode = input.required<fileNode>();
+
+  /**
+   * Used to set the item as selected
+   */
+  selectedItem() {}
+
+  /**
+   * Trys to go into a folder if it it is a directory or open the it if it is a file
+   */
+  goIntoItem() {
+    if (this.fileNode().isDirectory) {
+      ChangeActiveDirectory(this.fileNode().path, this.fileXContextService);
+    } else {
+      // open file - could try and open it in editor ? 
+    }
+  }
 }
