62970f75e7999ca728698be7a665e87977c8ac2a
Yousaf Wazir <umbrellaman612@gmail.com>
2026-02-10 15:50:39 +0000

    more

--- desktop/command-server/tpc-server.js
+++ desktop/command-server/tpc-server.js
@@ -1,48 +1,76 @@
 const net = require("node:net");
+const { logger } = require("../logger");
+
+
+// TODO: on data match cmd then broadcast to all
 
 /**
+ * Refrence to the TCP command server
  * @type {import("node:net").Server | null}
  */
 let server = null;
 
+/**
+ * Create a TCP command server
+ * @param {number} port The port to host the command server
+ * @param {string} host The host for the command server
+ * @returns Nothing
+ */
 function createCommandServer(port = 3214, host = "127.0.0.1") {
+  const envCommandPort = process.env["COMMAND_SERVER_PORT"];
+  if (!envCommandPort) {
+    logger.warn("ENV does not contain a COMMAND_SERVER_PORT value");
+  }
+  if (envCommandPort) {
+    port = Number(envCommandPort);
+  }
+
+  const envCommandHost = process.env["COMMAND_SERVER_HOST"];
+  if (!envCommandHost) {
+    logger.warn("ENV does not contain a COMMAND_SERVER_HOST");
+  }
+  if (envCommandHost) {
+    host = envCommandHost;
+  }
+
   if (server) {
     console.error("Server already running");
     return;
   }
 
   server = net.createServer((socket) => {
-    console.log(
+    logger.info(
       "Client connected:",
       socket.remoteAddress + ":" + socket.remotePort,
     );
 
     socket.on("data", (data) => {
-      console.log("Received:", data.toString());
+      logger.info("Received:", data.toString());
       socket.write("Server received: " + data);
     });
 
     socket.on("end", () => {
-      console.log("Client disconnected");
+      logger.info("Client disconnected");
     });
 
     socket.on("error", (err) => {
-      console.error("Socket error:", err.message);
+      logger.error("Socket error:", err);
     });
   });
 
   server.listen(port, host, () => {
-    console.log(`Server listening on ${host}:${port}`);
+    logger.info(`Server listening on ${host}:${port}`);
   });
 
   server.on("error", (err) => {
-    console.error("Server error:", err.message);
+    logger.error("Server error:", err);
     server = null;
   });
-
-  return server;
 }
 
+/**
+ * Stops the TCP command server
+ */
 function stopCommandServer() {
   if (!server) {
     console.error("Server not running");
@@ -50,7 +78,7 @@ function stopCommandServer() {
   }
 
   server.close(() => {
-    console.log("Server stopped");
+    logger.info("Server stopped");
     server = null;
   });
 }
