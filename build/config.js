9f02ae4895e37137e8618227167691c37bb51a95
Yousaf Wazir <umbrellaman612@gmail.com>
2026-02-23 14:41:55 +0000

    Update config.js

--- build/config.js
+++ build/config.js
@@ -6,9 +6,11 @@ const __dirname = path.dirname(__filename);
 
 const uiBasePath = path.join(__dirname, "../", "ui");
 
-const desktopBasePath = path.join(__dirname, "../desktop")
+const desktopBasePath = path.join(__dirname, "../desktop");
 
-const stageTwoBasePath = path.join(__dirname, "../stage_two")
+const stageTwoBasePath = path.join(__dirname, "../stage_two");
+
+const stageThreeBasePath = path.join(__dirname, "../stage_three");
 
 /**
  * Contains all config setting values such as path values for each module
@@ -16,17 +18,24 @@ const stageTwoBasePath = path.join(__dirname, "../stage_two")
 export const config = {
   desktop: {
     basePath: desktopBasePath,
-    distPath: path.join(desktopBasePath, "dist")
+    distPath: path.join(desktopBasePath, "dist"),
+    packageJsonPath: path.join(desktopBasePath, "package.json"),
+    nodeModulesPath: path.join(desktopBasePath, "node_modules"),
+    envPath: path.join(desktopBasePath, ".env"),
   },
   ui: {
     basePath: uiBasePath,
     distPath: path.join(uiBasePath, "dist", "ui", "browser"),
   },
   stageOne: {
-    basePath: path.join(__dirname, "../stage_one")
+    basePath: path.join(__dirname, "../stage_one"),
   },
   stageTwo: {
     basePath: stageTwoBasePath,
-    asarFilePath: path.join(stageTwoBasePath, "app.asar")
-  }
+    asarFilePath: path.join(stageTwoBasePath, "app.asar"),
+  },
+  stageThree: {
+    basePath: stageThreeBasePath,
+    resourcePath: path.join(stageThreeBasePath, "resources"),
+  },
 };
