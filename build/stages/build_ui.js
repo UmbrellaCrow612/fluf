685b9b7ad5b2c11d310f6332c7e7ba79e42ded21
Yousaf Wazir <umbrellaman612@gmail.com>
2026-02-26 12:48:27 +0000

    build UI

--- build/stages/build_ui.js
+++ build/stages/build_ui.js
@@ -1,61 +1,44 @@
 import { Logger } from "node-logy";
 import { promises } from "node:fs";
-import { createSafeRunOptions, nodeLogyOptions, safeExit } from "../utils.js";
+import { nodeLogyOptions, safeExit } from "../utils.js";
 import { config } from "../config.js";
 
 import { runCommand, safeRun } from "node-github-actions";
 
 const logger = new Logger(nodeLogyOptions);
 
-async function main() {
-  // Check if the UI base path exists
-  await safeRun(
-    async () => {
-      await promises.access(config.ui.basePath);
+safeRun(
+  async () => {
+    // Check if the UI base path exists
+    logger.info(`Checking if UI base path exists at ${config.ui.basePath}`);
+    await promises.access(config.ui.basePath);
+    logger.info("Path found");
+
+    // Build UI source code
+    logger.info(`Building UI source code at ${config.ui.basePath}`);
+    await runCommand("npm", ["run", "build"], { cwd: config.ui.basePath }, 60);
+    logger.info("UI build completed");
+
+    // Verify dist exists
+    logger.info(`Verifying UI dist exists at ${config.ui.distPath}`);
+    await promises.access(config.ui.distPath);
+    logger.info("Dist directory verified");
+
+    logger.info("UI source code built successfully");
+  },
+  {
+    exitFailCode: 1,
+    exitOnFailed: true,
+    onBefore: () => {
+      logger.info("Started UI build");
     },
-    createSafeRunOptions(
-      `Checking if UI base path exists at ${config.ui.basePath}`,
-      "Path found",
-      `UI base path does not exist: ${config.ui.basePath}`,
-      logger,
-    ),
-  );
-
-  // Build UI source code
-  await safeRun(
-    async () => {
-      await runCommand(
-        "npm",
-        ["run", "build"],
-        { cwd: config.ui.basePath },
-        60,
-      );
+    onAfter: async () => {
+      await safeExit(logger);
     },
-    createSafeRunOptions(
-      `Building UI source code at ${config.ui.basePath}`,
-      "UI build completed",
-      `Failed to build UI source code at ${config.ui.basePath}`,
-      logger,
-    ),
-  );
-
-  // Verify dist exists
-  await safeRun(
-    async () => {
-      await promises.access(config.ui.distPath);
+    onFail: async (err) => {
+      logger.error("UI build failed ", err);
+      await safeExit(logger);
     },
-    createSafeRunOptions(
-      `Verifying UI dist exists at ${config.ui.distPath}`,
-      "Dist directory verified",
-      `UI dist directory does not exist at ${config.ui.distPath}`,
-      logger,
-    ),
-  );
-
-  logger.info("UI source code built successfully");
-
-  // Exit
-  await safeExit(logger);
-}
-
-main();
+    timeoutMs: 3 * 60 * 1000,
+  },
+);
