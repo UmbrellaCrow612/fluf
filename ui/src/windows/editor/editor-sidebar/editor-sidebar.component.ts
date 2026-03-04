a59715b7a7abb629e0352fd2200de637fb6bd885
Yousaf Wazir <umbrellaman612@gmail.com>
2026-03-04 14:01:53 +0000

    Add active state

--- ui/src/windows/editor/editor-sidebar/editor-sidebar.component.ts
+++ ui/src/windows/editor/editor-sidebar/editor-sidebar.component.ts
@@ -1,11 +1,99 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal, Signal } from '@angular/core';
+import { sideBarActiveElement } from '../editor-context/type';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltip } from '@angular/material/tooltip';
+import { EditorContextService } from '../editor-context/editor-context.service';
 
+/**
+ * Represents a clickable side bar element
+ */
+type editorSideBarItem = {
+  /**
+   * The specific element to render on click
+   */
+  element: sideBarActiveElement;
+
+  /**
+   * Hover information
+   */
+  tooltip: string;
+
+  /**
+   * Angular material icon
+   */
+  icon: string;
+};
+
+/**
+ * Renders a small widget on the left to allow users to switch which active component to render i.e file explorer etc
+ */
 @Component({
   selector: 'app-editor-sidebar',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatTooltip],
   templateUrl: './editor-sidebar.component.html',
   styleUrl: './editor-sidebar.component.css',
 })
 export class EditorSidebarComponent {
+  private readonly editorContextService = inject(EditorContextService);
 
+  /**
+   * Keeps track of the current active side bar element
+   */
+  public activeElement = computed(() =>
+    this.editorContextService.sideBarActiveElement(),
+  );
+
+  /**
+   * Select a side bar item for the editor state
+   * @param item The item to select in the editor state
+   * @returns Nothing
+   */
+  public selectSidebarItem(item: editorSideBarItem) {
+    const newValue =
+      this.activeElement() === item.element ? null : item.element;
+
+    this.editorContextService.sideBarActiveElement.set(newValue);
+  }
+
+  /**
+   * List of items that are clickable to switch active component in the side bar
+   */
+  public sideBarElements: Signal<editorSideBarItem[]> = signal([
+    {
+      element: 'file-explorer',
+      tooltip: 'File explorer',
+      icon: 'file_copy',
+    },
+    {
+      element: 'search',
+      tooltip: 'Search',
+      icon: 'search',
+    },
+    {
+      element: 'search-folders',
+      tooltip: 'Search folders',
+      icon: 'tab_search',
+    },
+    {
+      element: 'search-files',
+      tooltip: 'Search files',
+      icon: 'find_in_page',
+    },
+    {
+      element: 'source-control',
+      tooltip: 'Source control',
+      icon: 'change_history',
+    },
+    {
+      element: 'run-and-debug',
+      tooltip: 'Run and debug',
+      icon: 'bug_report',
+    },
+    {
+      element: 'extensions',
+      tooltip: 'Extensions',
+      icon: 'extension',
+    },
+  ]);
 }
