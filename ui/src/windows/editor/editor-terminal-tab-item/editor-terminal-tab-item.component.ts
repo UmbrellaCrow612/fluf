963f526e9a53c746ee80e4283389bf499465e379
Yousaf Wazir <umbrellaman612@gmail.com>
2026-03-16 16:28:59 +0000

    more

--- ui/src/windows/editor/editor-terminal-tab-item/editor-terminal-tab-item.component.ts
+++ ui/src/windows/editor/editor-terminal-tab-item/editor-terminal-tab-item.component.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-editor-terminal-tab-item',
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatTooltipModule],
   templateUrl: './editor-terminal-tab-item.component.html',
   styleUrl: './editor-terminal-tab-item.component.css',
 })
--- ui/src/windows/editor/editor-terminal-tabs/editor-terminal-tabs.component.ts
+++ ui/src/windows/editor/editor-terminal-tabs/editor-terminal-tabs.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
-import { EditorTerminalTabItemComponent } from "../editor-terminal-tab-item/editor-terminal-tab-item.component";
+import { Component, OnInit } from '@angular/core';
+import { EditorTerminalTabItemComponent } from '../editor-terminal-tab-item/editor-terminal-tab-item.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 /**
  * Holds the active tabs and allows crud operations on them
  */
 @Component({
   selector: 'app-editor-terminal-tabs',
-  imports: [EditorTerminalTabItemComponent],
+  imports: [
+    EditorTerminalTabItemComponent,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+  ],
   templateUrl: './editor-terminal-tabs.component.html',
   styleUrl: './editor-terminal-tabs.component.css',
 })
-export class EditorTerminalTabsComponent {
-
+export class EditorTerminalTabsComponent implements OnInit {
+  ngOnInit(): void {
+   
+  }
 }
