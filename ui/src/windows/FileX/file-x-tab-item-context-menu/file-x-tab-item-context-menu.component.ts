4fb1e43ea7385efcf3a9e185ee02edad35d6f56f
Yousaf Wazir <umbrellaman612@gmail.com>
2026-04-12 12:44:48 +0100

    removed file x

--- ui/src/windows/FileX/file-x-tab-item-context-menu/file-x-tab-item-context-menu.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-file-x-tab-item-context-menu',
-  imports: [],
-  templateUrl: './file-x-tab-item-context-menu.component.html',
-  styleUrl: './file-x-tab-item-context-menu.component.css',
-})
-export class FileXTabItemContextMenuComponent {}
--- ui/src/windows/FileX/store-key-constants.ts
+++
@@ -1,4 +0,0 @@
-/**
- * Used as the key in which we store, update and retrieve the session data for file x between browsers
- */
-export const FILE_X_STORE_DATA = 'file_x_store_data';
