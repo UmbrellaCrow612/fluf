e6d74be59dff5083063c5cb111009b0aa58076e2
Yousaf Wazir <107571370+UmbrellaCrow612@users.noreply.github.com>
2026-04-23 15:48:19 +0100

    spawn custom exes for terminal shells (#154)

--- ui/src/windows/editor/editor-terminal-tabs/editor-terminal-tabs.component.ts
+++ ui/src/windows/editor/editor-terminal-tabs/editor-terminal-tabs.component.ts
@@ -1,9 +1,12 @@
-import { Component, inject, signal } from "@angular/core";
+import { Component, inject, OnInit, signal } from "@angular/core";
 import { EditorTerminalTabItemComponent } from "../editor-terminal-tab-item/editor-terminal-tab-item.component";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { MatTooltipModule } from "@angular/material/tooltip";
 import { EditorTerminalService } from "../core/terminal/editor-terminal.service";
+import { MatMenuModule } from "@angular/material/menu";
+import { getElectronApi } from "../../../shared/electron";
+import { shellExecutableInformation } from "../../../gen/type";
 
 /**
  * Holds the active tabs and allows crud operations on them
@@ -15,12 +18,14 @@ import { EditorTerminalService } from "../core/terminal/editor-terminal.service"
     MatIconModule,
     MatButtonModule,
     MatTooltipModule,
+    MatMenuModule,
   ],
   templateUrl: "./editor-terminal-tabs.component.html",
   styleUrl: "./editor-terminal-tabs.component.css",
 })
-export class EditorTerminalTabsComponent {
+export class EditorTerminalTabsComponent implements OnInit {
   private readonly editorTerminalService = inject(EditorTerminalService);
+  private readonly electronApi = getElectronApi();
 
   /**
    * Holds state for creating terminal
@@ -33,13 +38,38 @@ export class EditorTerminalTabsComponent {
   public readonly activeShells = this.editorTerminalService.shellPidInfoMap;
 
   /**
-   * Creats a new terminal in the selected directory path or default path if it not defined
+   * List of spawnable shells
    */
-  public async createNewTerminal() {
+  public readonly shellExecutableInformations = signal<
+    shellExecutableInformation[]
+  >([]);
+
+  public ngOnInit() {
+    void this.fetchAndSetSpawnableExeOptions();
+  }
+
+  /**
+   * Fetches the spawnable shell exes
+   */
+  private async fetchAndSetSpawnableExeOptions() {
+    try {
+      const options =
+        await this.electronApi.shellApi.getShellSpawnExecutables();
+      this.shellExecutableInformations.set(options);
+    } catch (error) {
+      console.error("Failed to get shell options ", error);
+    }
+  }
+
+  /**
+   * Creats a new terminal in the selected directory path or default path if it not defined
+   * @param [exe=undefined] Custom exe to spawn
+   */
+  public async createNewTerminal(exe: string | undefined = undefined) {
     try {
       this.isCreatingTerminal.set(true);
 
-      const res = await this.editorTerminalService.createShell(); // tood allow dropdown select specific shell when creating
+      const res = await this.editorTerminalService.createShell(exe); // tood allow dropdown select specific shell when creating
       if (!res.successed) {
         throw new Error(res.reason);
       }
